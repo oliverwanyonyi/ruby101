a7fa95fd33db583e8fd62e62ff9b67e3e1d0f358
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2025-02-17 20:09:28 +0300

    lists

---
+++ datastrucutres/lists.rb
@@ -0,0 +1,20 @@
+
+
+fruits = ['apple', 'banana','pineapple','orange']
+
+#puts fruits[0] #apple
+
+fruits.push('mango')
+
+# puts fruits
+
+fruits.pop()
+
+# puts fruits
+
+#fruits.shift()
+#puts fruits
+
+fruits.unshift('watermelon')
+
+# puts fruits
