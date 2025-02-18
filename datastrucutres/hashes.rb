16774831f4e0e48a285757cfad2c9194c7aab404
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2025-02-18 20:30:39 +0300

    hashes in ruby

---
+++ datastrucutres/hashes.rb
@@ -0,0 +1,11 @@
+#creating a hash
+
+# institution = {"name" => "UON", "type"=>"public", "location" => 'nairobi'}
+#accessing items
+#puts institution["type"]
+
+#or
+
+institution = { name: "UON", type:"Public", location:"Nairobi"}
+
+puts institution[:name]
