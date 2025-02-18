6fc5dacf6678cb26da4242641b0e0ee8feb44b23
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2025-02-18 20:49:35 +0300

    set operation

--- datastrucutres/sets.rb
+++ datastrucutres/sets.rb
@@ -1,4 +1,11 @@
 #initializing a set(unique collection of elements)
 
 fruits = Set.new(['mango', 'apple', 'pineapple', 'banana', 'mango'])
+# puts fruits
+
+#adding and removing items from a set
+
+fruits.add("watermelon")
+fruits.delete('apple')
+
 puts fruits
